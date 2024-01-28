c6502edf9e6a16b72d81352d8e724bb548bccf78
Hasib Al Asad <Hasibaveiro007@gmail.com>
2024-01-28 22:51:47 +0600

    dropdown added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import Logo from "../../assets/logo.png"
 import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import DarkMode from "./DarkMode";
 
 const Menu = [
@@ -31,6 +31,24 @@ const Menu = [
     }
 ]
 
+const DropdownLinks = [
+    {
+        id: 1,
+        name: "Best Sellers",
+        link: "/#"
+    },
+    {
+        id: 2,
+        name: "Top Rated",
+        link: "/#"
+    },
+    {
+        id: 3,
+        name: "New Arrivals",
+        link: "/#"
+    }
+]
+
 function Navbar() {
     return (
         <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white relative z-10">
@@ -50,7 +68,7 @@ function Navbar() {
                             <input
                                 type="text"
                                 placeholder="Search"
-                                className="w-[200px] sm:w-[200px] group-hover:w-[300px] border border-gray-300 transition-all duration-300 rounded-full px-2 py-1 focus:outline-none focus:border-primary " />
+                                className="w-[200px] sm:w-[200px] group-hover:w-[300px] border border-gray-300 transition-all duration-300 rounded-full px-2 py-1 focus:outline-none focus:border-primary dark:bg-gray-800 dark:border-gray-500" />
                             <IoMdSearch className=" text-gray-500 group-hover:text-primary absolute top-1/2 right-3 -translate-y-1/2" />
                         </div>
 
@@ -75,17 +93,33 @@ function Navbar() {
             <div className="flex justify-center ">
                 <ul className="flex items-center gap-4">
                     {
-                        Menu.map((item) => {
-                            return (
-                                <li key={item.id} className="">
-                                    <a href={item.link} className=" inline-block px-4 py-2 hover:text-primary transition-all duration-300">{item.name}</a>
-                                </li>
-                            )
-                        })
+                        Menu.map((item) => (
+                            <li key={item.id} >
+                                <a
+                                    href={item.link}
+                                    className=" inline-block px-4 py-2 hover:text-primary transition-all duration-300">{item.name}</a>
+                            </li>
+
+                        ))
                     }
                     {/* dropdown menu */}
-                    <li>
-                        <a href="" className=" inline-block px-4 py-2 hover:text-primary transition-all duration-300">Trending Items</a>
+                    <li className="group relative cursor-pointer">
+                        <a href="#" className="flex items-center gap-[2px] py-2">
+                            Trending Items
+                            <span className="group-hover:rotate-180 transition-all duration-300">
+                                <FaCaretDown />
+                            </span>
+                        </a>
+                        <div className="absolute hidden z-50 group-hover:block bg-white dark:bg-gray-900 rounded-md shadow-md w-[200px] p-2 transition-all duration-300">
+                            <ul>
+                                {DropdownLinks.map((item) => (
+                                    <li key={item.id}>
+                                        <a href={item.link} className="inline-block w-full rounded-md p-2 hover:bg-primary/20">{item.name}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+
                     </li>
                 </ul>
             </div>
